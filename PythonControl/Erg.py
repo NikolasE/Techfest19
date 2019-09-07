f309a4d8ea2251847a8dde8d090a37b790557cde
Laurenz <github@laure.nz>
2019-09-07 17:18:45 +0200

    add percent_complete for erg

--- PythonControl/Erg.py
+++ PythonControl/Erg.py
@@ -4,6 +4,7 @@
 import threading
 from pyrow import pyrow
 from pyrow.pyrow import PyErg
+import time
 
 
 class Erg():
@@ -11,6 +12,9 @@ class Erg():
         ergs = list(pyrow.find())
         self._erg = PyErg(ergs[0])
         self._erg_dict = {}
+        self._stroke_dwelling_stamp = time.time()
+        self.__last_stroke_state = -1
+        self._stroke_period = 5.0
         self._erg_thread = threading.Thread(
             target=self._run_thread, daemon=True)
         self._erg_dict = self._erg.get_monitor(forceplot=True)  # populate dict
@@ -19,6 +23,20 @@ class Erg():
     def _run_thread(self):
         while True:
             self._erg_dict = self._erg.get_monitor(forceplot=True)
+            stroke_state = self._erg_dict['strokestate']
+            if 'strokestate' in self._erg_dict.keys() and stroke_state == 3 and self.__last_stroke_state == 2:
+                now = time.time()
+                period = now - self._stroke_dwelling_stamp
+                # if period > 1.0:
+                if True:
+                    self._stroke_period = period
+                    self._stroke_dwelling_stamp = now
+            self.__last_stroke_state = stroke_state
 
     def get_distance(self):
         return self._erg_dict['distance']
+
+    def get_current_stroke_complete_percent(self):
+        current_stroke_elapsed_time = time.time() - self._stroke_dwelling_stamp
+        return current_stroke_elapsed_time / self._stroke_period
+
--- PythonControl/Main.py
+++ PythonControl/Main.py
@@ -77,10 +77,15 @@ while True:
     # erg distance
     dist = erg.get_distance()
     dist_expected = (time.time() - workout_start) * dist_per_time_needed
-    print(dist, dist_expected)
+    # print(dist, dist_expected)
     dist_leds = round(valmap(dist - dist_expected, 0, 10, 0, 60))
     if last_dist_leds != dist_leds:
         lc.set_dist(dist_leds)
         last_dist_leds = dist_leds
 
+    # show setpoints
+    percent_complete = erg.get_current_stroke_complete_percent()
+    # print(percent_complete, '%, ', erg._stroke_period, 's')
+
     time.sleep(0.05)
+
